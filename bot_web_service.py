16da1d1d31f33f19e61ba60a2dc708a8e371c2d1
MarianoBidermanGrisi <marianogrisi@gmail.com>
2025-10-22 21:03:19 -0300

    Fix formatting issues in bot_web_service.py

--- bot_web_service.py
+++ bot_web_service.py
@@ -2747,5 +2747,5 @@ if __name__ == '__main__':
     bot = TradingBot(auto_optimize=True)
     port = int(os.environ.get('PORT', 5000))
     logger.info(f"Aplicación iniciada en puerto {port}")
->>>>>>> b64ec1fe6d27bd204ea69bffac4791dda7ce2a9d
+
     app.run(host='0.0.0.0', port=port)
