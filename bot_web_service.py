18354ae70ead5f1795b2dcce1d2e2cb52b234088
MarianoBidermanGrisi <marianogrisi@gmail.com>
2025-12-13 03:13:43 -0300

    Update bot_web_service.py

--- bot_web_service.py
+++ bot_web_service.py
@@ -1254,7 +1254,7 @@ class TradingBot:
         else:
             print(f"     📢 Breakout detectado en {simbolo} (sin Telegram)")
 
-rafico_breakout    def generar_g(self, simbolo, info_canal, datos_mercado, tipo_breakout, config_optima):
+        def generar_grafico_breakout(self, simbolo, info_canal, datos_mercado, tipo_breakout, config_optima):
         """
         Genera gráfico especial para el momento del BREAKOUT
         Marca visualmente la ruptura del canal
