eee14675059e0493a7226555a4b43e577fb593a0
MarianoBidermanGrisi <marianogrisi@gmail.com>
2026-02-07 20:52:15 -0300

    Fix indentation in reentry symbol processing loop

--- bot_web_service.py
+++ bot_web_service.py
@@ -4052,7 +4052,8 @@ class TradingBot:
         eliminados = 0
         simbolos_a_eliminar = []
     
-        for simbolo, info in self.esperando_reentry.items():tiempo_espera = (datetime.now() - info['timestamp']).total_seconds() / 60
+        for simbolo, info in self.esperando_reentry.items():
+            tiempo_espera = (datetime.now() - info['timestamp']).total_seconds() / 60
             if tiempo_espera > max_wait_minutes:
                 simbolos_a_eliminar.append(simbolo)
                 eliminados += 1
