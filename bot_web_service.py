f29ee48d0a85e7c28f516ffa870fbb80f3ffed62
MarianoBidermanGrisi <marianogrisi@gmail.com>
2026-02-26 20:33:39 -0300

    Update bot_web_service.py

--- bot_web_service.py
+++ bot_web_service.py
@@ -7,6 +7,7 @@ import sys
 import json
 from datetime import datetime
 from decimal import Decimal, ROUND_DOWN, ROUND_HALF_UP
+from flask import Flask, request, jsonify
 # import matplotlib  # Eliminado - no se usa y causa conflictos en Render
 # from io import BytesIO  # Eliminado - no se usa
 import threading
